f16148f5a3b540ed5614e92616f0929c189563ac
Israel Teixeira <israelst@gmail.com>
2020-02-29 16:05:26 -0300

    Atualização da instalação do driver do Firefox

--- install-gecko-driver.sh
+++ install-gecko-driver.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-wget https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz
-tar xvfz geckodriver-v0.19.1-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+tar xvfz geckodriver-v0.26.0-linux64.tar.gz
+mkdir -p ~/.local/bin
 mv geckodriver ~/.local/bin
-rm geckodriver-v0.19.1-linux64.tar.gz
+rm geckodriver-v0.26.0-linux64.tar.gz
